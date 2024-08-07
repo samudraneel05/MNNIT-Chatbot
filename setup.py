3b04d256bf5ae405bebd14a52a6d78dd2e9a816b
Samudraneel Sarkar <98489891+samudraneel05@users.noreply.github.com>
2024-08-07 20:01:24 +0000

    Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MNNIT Chatbot',
+    version= '0.0.0',
+    author= 'Samudraneel Sarkar',
+    author_email= 'samudraneel05@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
